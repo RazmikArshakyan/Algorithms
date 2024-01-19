e890545cb41ea25e556917649e9ed089ad113a9f
RazmikArshakyan <89875456+RazmikArshakyan@users.noreply.github.com>
2024-01-19 18:11:48 +0400

    Update main.cpp

--- Graphs/main.cpp
+++ Graphs/main.cpp
@@ -133,105 +133,116 @@ int main() {
 
     // std::cout << g10.is_cyclic_directed();
 
-    std::cout << "\nSorting graph in topological order\n";
-    Graph g11(5);
-    g11.add_edge(0, 1);
-    g11.add_edge(1, 2);
-    g11.add_edge(2, 3);
-    g11.add_edge(3, 4);
-    g11.add_edge(4, 1);
+    // std::cout << "\nSorting graph in topological order\n";
+    // Graph g11(5);
+    // g11.add_edge(0, 1);
+    // g11.add_edge(1, 2);
+    // g11.add_edge(2, 3);
+    // g11.add_edge(3, 4);
+    // g11.add_edge(4, 1);
 
-    std::vector<int> ts1 = g11.topological_sort();
-    for (auto it : ts1)
-        std::cout << it;
+    // std::vector<int> ts1 = g11.topological_sort();
+    // for (auto it : ts1)
+    //     std::cout << it;
 
-    std::cout << "Another example\n";
-    std::cout << "Sorting graph in topological order\n";
-    Graph g12(6);
-    g12.add_edge(0, 1);
-    g12.add_edge(0, 2);
-    g12.add_edge(2, 3);
-    g12.add_edge(3, 1);
-    g12.add_edge(1, 4);
-    g12.add_edge(3, 4);
-    g12.add_edge(4, 5);
+    // std::cout << "Another example\n";
+    // std::cout << "Sorting graph in topological order\n";
+    // Graph g12(6);
+    // g12.add_edge(0, 1);
+    // g12.add_edge(0, 2);
+    // g12.add_edge(2, 3);
+    // g12.add_edge(3, 1);
+    // g12.add_edge(1, 4);
+    // g12.add_edge(3, 4);
+    // g12.add_edge(4, 5);
 
-    std::vector<int> ts2 = g12.topological_sort();
-    for (auto elem : ts2)
-        std::cout << elem  << " ";
+    // std::vector<int> ts2 = g12.topological_sort();
+    // for (auto elem : ts2)
+    //     std::cout << elem  << " ";
 
-    std::cout << "\nSorting graph in topological order with Kahn's algorithm\n";
-    Graph g13(4);
-    g13.add_edge(0, 1);
-    g13.add_edge(1, 2);
-    g13.add_edge(2, 3);
-    g13.add_edge(3, 0);
+    // std::cout << "\nSorting graph in topological order with Kahn's algorithm\n";
+    // Graph g13(4);
+    // g13.add_edge(0, 1);
+    // g13.add_edge(1, 2);
+    // g13.add_edge(2, 3);
+    // g13.add_edge(3, 0);
 
-    std::vector<int> ts3 = g13.kahns_algorithm();
+    // std::vector<int> ts3 = g13.kahns_algorithm();
 
-    std::cout << "Another check\n";
-    std::cout << "Sorting graph in topological order with Kahn's algorithm\n";
-    Graph g14(6);
-    g14.add_edge(5, 0);
-    g14.add_edge(4, 0);
-    g14.add_edge(5, 2);
-    g14.add_edge(2, 3);
-    g14.add_edge(3, 1);
-    g14.add_edge(4, 1);
+    // std::cout << "Another check\n";
+    // std::cout << "Sorting graph in topological order with Kahn's algorithm\n";
+    // Graph g14(6);
+    // g14.add_edge(5, 0);
+    // g14.add_edge(4, 0);
+    // g14.add_edge(5, 2);
+    // g14.add_edge(2, 3);
+    // g14.add_edge(3, 1);
+    // g14.add_edge(4, 1);
 
-    std::vector<int> ts4 = g14.kahns_algorithm();
-    for (auto num : ts4) 
-        std::cout << num << " ";
+    // std::vector<int> ts4 = g14.kahns_algorithm();
+    // for (auto num : ts4) 
+    //     std::cout << num << " ";
 
-    std::cout << "\nFinding SCCs of graph\n";
-    Graph g15(5);
-    g15.add_edge(0, 2);
-    g15.add_edge(2, 1);
-    g15.add_edge(1, 0);
-    g15.add_edge(0, 3);
-    g15.add_edge(3, 4);
+    // std::cout << "\nFinding SCCs of graph\n";
+    // Graph g15(5);
+    // g15.add_edge(0, 2);
+    // g15.add_edge(2, 1);
+    // g15.add_edge(1, 0);
+    // g15.add_edge(0, 3);
+    // g15.add_edge(3, 4);
 
-    // Not recommended as time complexity is O(N^3)
-    std::vector<std::vector<int>> sccs1 = g15.brute_force_scc();
-    for (auto scc : sccs1) {
-        for (auto vertex : scc)
-            std::cout << vertex << " ";
-        std::cout << std::endl;
-    }
+    // // Not recommended as time complexity is O(N^3)
+    // std::vector<std::vector<int>> sccs1 = g15.brute_force_scc();
+    // for (auto scc : sccs1) {
+    //     for (auto vertex : scc)
+    //         std::cout << vertex << " ";
+    //     std::cout << std::endl;
+    // }
 
-    std::cout << "Finding SCCs, using Kosarajus algorithm\n";
-    Graph g16(7);
-    g16.add_edge(0, 2);
-    g16.add_edge(2, 3);
-    g16.add_edge(3, 1);
-    g16.add_edge(1, 0);
-    g16.add_edge(1, 4);
-    g16.add_edge(4, 5);
-    g16.add_edge(5, 4);
-    g16.add_edge(5, 6);
-    g16.add_edge(3, 5);
+    // std::cout << "Finding SCCs, using Kosarajus algorithm\n";
+    // Graph g16(7);
+    // g16.add_edge(0, 2);
+    // g16.add_edge(2, 3);
+    // g16.add_edge(3, 1);
+    // g16.add_edge(1, 0);
+    // g16.add_edge(1, 4);
+    // g16.add_edge(4, 5);
+    // g16.add_edge(5, 4);
+    // g16.add_edge(5, 6);
+    // g16.add_edge(3, 5);
 
-    std::vector<std::vector<int>> sccs2 = g16.kosaraju_scc();
-    for (auto scc : sccs2) {
-        for (auto vertex : scc)
-            std::cout << vertex << " ";
-        std::cout << std::endl;
-    }   
+    // std::vector<std::vector<int>> sccs2 = g16.kosaraju_scc();
+    // for (auto scc : sccs2) {
+    //     for (auto vertex : scc)
+    //         std::cout << vertex << " ";
+    //     std::cout << std::endl;
+    // }   
     
-    std::cout << "Another test\n";
-    Graph g17(6);
-    g17.add_edge(0, 1);
-    g17.add_edge(1, 2);
-    g17.add_edge(2, 0);
-    g17.add_edge(3, 4);
-    g17.add_edge(4, 3);
+    // std::cout << "Another test\n";
+    // Graph g17(6);
+    // g17.add_edge(0, 1);
+    // g17.add_edge(1, 2);
+    // g17.add_edge(2, 0);
+    // g17.add_edge(3, 4);
+    // g17.add_edge(4, 3);
 
-    std::vector<std::vector<int>> sccs3 = g17.kosaraju_scc();
-    for (auto scc : sccs3) {
-        for (auto vertex : scc)
-            std::cout << vertex << " ";
-        std::cout << std::endl;
-    }    
+    // std::vector<std::vector<int>> sccs3 = g17.kosaraju_scc();
+    // for (auto scc : sccs3) {
+    //     for (auto vertex : scc)
+    //         std::cout << vertex << " ";
+    //     std::cout << std::endl;
+    // }
+
+    Graph g18(6);
+    g18.add_edge(0, 1);
+    g18.add_edge(0, 2);
+    g18.add_edge(2, 4);
+    g18.add_edge(3, 1);
+    g18.add_edge(4, 5);
+
+    std::vector<int> aps1 = g18.articulation_points();
+    for (auto ap : aps1) 
+        std::cout << ap << " ";
 
     return 0;
 }
