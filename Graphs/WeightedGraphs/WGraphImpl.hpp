41e217d4cc00985eb11451ba0781a8ab950d40b0
RazmikArshakyan <89875456+RazmikArshakyan@users.noreply.github.com>
2024-01-25 00:04:19 +0400

    Update WGraphImpl.hpp

--- Graphs/WeightedGraphs/WGraphImpl.hpp
+++ Graphs/WeightedGraphs/WGraphImpl.hpp
@@ -8,7 +8,7 @@ WGraph::WGraph(int vertices)
 inline void WGraph::add_edge(int u, int v, int w)
 {
     adjacency_list[u].push_back({v, w});
-    adjacency_list[v].push_back({u, w}); // for undirected graphs
+    // adjacency_list[v].push_back({u, w}); // for undirected graphs
 }
 
 inline int WGraph::mst_prim(int src)
@@ -20,8 +20,8 @@ inline int WGraph::mst_prim(int src)
 
     pq.push({0, src});
     while (!pq.empty()) {
-        auto vertex = pq.top().second;
-        auto weight = pq.top().first;
+        auto[weight, vertex] = pq.top();
+
         pq.pop();
         
         if (visited[vertex])
@@ -49,6 +49,107 @@ inline int WGraph::mst_kruskal()
         }
     }
     return total;
+}
+
+inline std::vector<int> WGraph::top_sort()
+{
+    std::vector<int> indegree(vertices, 0);
+    std::queue<int> q;
+    std::vector<int> topological_order;
+
+    for (int i = 0; i < vertices; ++i)
+        for (auto vertex : adjacency_list[i])
+            ++indegree[vertex.first];
+    
+    for (int i = 0; i < vertices; ++i)
+        if (indegree[i] == 0)
+            q.push(i);
+    
+    while (!q.empty()) {
+        auto vertex = q.front();
+        q.pop();
+
+        topological_order.push_back(vertex);
+
+        for (auto v : adjacency_list[vertex])
+            if (--indegree[v.first] == 0)
+                q.push(v.first);
+    }
+
+    if (topological_order.size() == vertices)
+        return topological_order;
+    return {};
+}
+
+inline std::vector<int> WGraph::sssp_dag(int src)
+{
+    std::vector<int> dist(vertices, INT_MAX);
+    
+    auto sorted = top_sort();
+    dist[src] = 0;
+
+    for (auto elem : sorted)
+        if (dist[elem] != INT_MAX)
+            for (auto vertex : adjacency_list[elem])
+                if (dist[vertex.first] > dist[elem] + vertex.second)
+                    dist[vertex.first] = dist[elem] + vertex.second;
+    return dist;
+}
+
+inline std::vector<int> WGraph::sssp_dijkstra(int src)
+{
+    std::vector<int> dist(vertices, INT_MAX);
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>,
+        std::greater<std::pair<int, int>>> pq;
+    std::vector<bool> visited(vertices, false);
+
+    dist[src] = 0;
+    pq.push({src, 0});
+
+    while (!pq.empty()) {
+        auto[vertex, weight] = pq.top();
+        pq.pop();
+
+        if (dist[vertex] < weight)
+            continue;
+        visited[vertex] = true;
+
+        for (auto v : adjacency_list[vertex]) {
+            if (!visited[v.first]) {
+                if (dist[v.first] > dist[vertex] + v.second) {
+                    dist[v.first] = dist[vertex] + v.second;
+                    pq.push({v.first, dist[v.first]});
+                }
+            }
+        }        
+    }
+    return dist;
+}
+
+inline std::vector<int> WGraph::sssp_bellman_ford(int src)
+{
+    std::vector<int> dist(vertices, INT_MAX);
+    dist[src] = 0;
+
+    for (int i = 1; i < vertices; ++i) {
+        for (int j = 0; j < vertices; ++j) {
+            for (auto vertex : adjacency_list[j]) {
+                if (dist[vertex.first] > dist[j] + vertex.second) {
+                    dist[vertex.first] = dist[j] + vertex.second;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < vertices; ++i) {
+        for (auto vertex : adjacency_list[i]) {
+            if (dist[vertex.first] > dist[i] + vertex.second) {
+                std::cout << "Ill formed graph\n";
+                return {};
+            }
+        }
+    }
+    return dist;
 }
 
 inline std::vector<std::vector<int>> WGraph::adjacency_list_to_edges()
