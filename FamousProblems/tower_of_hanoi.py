870d8cad5eaf2eb0d9569ba80bd25aab5c5fdce9
RazmikArshakyan <89875456+RazmikArshakyan@users.noreply.github.com>
2024-03-08 21:23:47 +0400

    Update tower_of_hanoi.py

--- FamousProblems/tower_of_hanoi.py
+++ FamousProblems/tower_of_hanoi.py
@@ -2,8 +2,9 @@ def tower_of_hanoi(n, from_r, to_r, aux_r):
     if n == 0:
         return
     tower_of_hanoi(n - 1, from_r, aux_r, to_r)
-    print("move circle", n, "from r", from_r, "to r", to_r)
+    print("move circle", n, "from rod", from_r, "to rod", to_r)
     tower_of_hanoi(n - 1, aux_r, to_r, from_r)
 
-n = 3
-tower_of_hanoi(n, 'A', 'C', 'B')
+if __name__ == '__main__':
+    n = 3
+    tower_of_hanoi(n, 'A', 'C', 'B')
